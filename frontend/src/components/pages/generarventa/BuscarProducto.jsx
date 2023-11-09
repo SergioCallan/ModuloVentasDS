269cb1e8055131755805d825f9b9fa9c30c07737
ALEX241342 <axel.felix@unmsm.edu.pe>
2023-11-09 14:25:46 -0500

    Borrando archivos innecesarios y corrigiendo error

--- frontend/src/components/pages/generarventa/BuscarProducto.jsx
+++ frontend/src/components/pages/generarventa/BuscarProducto.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Sidebar from "../../extras/Sidebar";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
