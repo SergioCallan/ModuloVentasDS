77e551c81cf4d3dfbeb20eb7ebdba196aa5228fe
SergioCallan <sergio.callan@unmsm.edu.pe>
2023-11-30 00:11:26 -0500

    Cambios en sellDetails.controllers

--- backend/controllers/sellDetails.controllers.js
+++ backend/controllers/sellDetails.controllers.js
@@ -1,28 +1,34 @@
-const pool= require ('../db')
+const pool = require('../db');
+const axios = require('axios');
 
 const getSellAndClientDetails = async (req, res) => {
   try {
     const { id_detalle } = req.params;
-    const sellDetailsQuery = `
-      SELECT dv.*, v.dni_cliente 
-      FROM detalleventa dv 
-      INNER JOIN venta v ON dv.id_venta = v.id_venta 
-      WHERE dv.id_detalle = $1`;
+    
+    // Primero, obtén el id_venta basado en el id_detalle
+    const sellDetailsQuery = 'SELECT * FROM detalleventa WHERE id_detalle = $1';
     const sellDetailsResult = await pool.query(sellDetailsQuery, [id_detalle]);
 
     if (sellDetailsResult.rows.length === 0) {
       return res.status(404).json({ message: 'Detalle de venta no encontrado.' });
     }
 
-    const sellDetails = sellDetailsResult.rows[0];
-    const dniCliente = sellDetails.dni_cliente;
+    // Después, usa id_venta para obtener dni_cliente de la tabla venta
+    const id_venta = sellDetailsResult.rows[0].id_venta;
+    const ventaQuery = 'SELECT dni_cliente FROM venta WHERE id_venta = $1';
+    const ventaResult = await pool.query(ventaQuery, [id_venta]);
 
+    if (ventaResult.rows.length === 0) {
+      return res.status(404).json({ message: 'Venta no encontrada.' });
+    }
+
+    const dniCliente = ventaResult.rows[0].dni_cliente;
     if (!dniCliente) {
       return res.status(404).json({ message: 'DNI del cliente no encontrado.' });
     }
 
+    // Finalmente, con el dni_cliente obtén la información del cliente de la API externa
     const clientResponse = await axios.get(`https://clientemodulocrm.onrender.com/clientes/buscarPorDNI/${dniCliente}`);
-
     if (!clientResponse.data) {
       return res.status(404).json({ message: 'Datos del cliente no encontrados.' });
     }
@@ -35,8 +41,9 @@ const getSellAndClientDetails = async (req, res) => {
       // Incluye aquí otros campos que necesites
     };
 
+    // Combina los detalles de venta con la información del cliente y envía la respuesta
     const responseData = {
-      ...sellDetails,
+      detalleVenta: sellDetailsResult.rows[0],
       cliente: cliente
     };
 
@@ -46,6 +53,7 @@ const getSellAndClientDetails = async (req, res) => {
     res.status(500).json({ error: 'Error interno del servidor' });
   }
 };
-module.exports={
+
+module.exports = {
   getSellAndClientDetails
-}
+};
