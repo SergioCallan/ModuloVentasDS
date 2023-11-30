66db907da82bcf45b27e93fccc28c9b1805175f9
SergioCallan <sergio.callan@unmsm.edu.pe>
2023-11-30 12:21:04 -0500

    Ver detalles terminado.

--- frontend/src/components/pages/reporteventas/estadoReporteVenta.jsx
+++ frontend/src/components/pages/reporteventas/estadoReporteVenta.jsx
@@ -1,19 +1,23 @@
 // Cambiar a medida que se avance
 class EstadoBase {
+    buscarDatosDia(numero_linea, id) {
+        throw new Error("Método pagar no implementado.");
+      }
     
+    buscarDatosSemana(numero_linea, id) {
+        throw new Error("Método suspender no implementado.");
+      }
+    
+    buscarDatosMes(numero_linea, id) {
+        throw new Error("Método cancelar no implementado.");
+      }
 }
 
 // EstadoPendiente.js
 class General extends EstadoBase {
   async buscarDatosDia(tiempo, periodo1, periodo2){
     //TODO: Darle logica a buscar datos, agregar las graficas y arreglar todo el codigo
-    try{
-        const query= "SELECT SUM(monto) AS total, fecha FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY fecha ORDER BY fecha"
-        const resultVentas= await pool.query(query, [periodo1, periodo2])
-        return resultVentas.rows
-    }catch(error){
-        console.error("Error al buscar datos: ", error)
-    }
+    
   }
   async buscarDatosSemana(tiempo, periodo1, periodo2){
 //TODO: Darle logica a buscar datos, agregar las graficas y arreglar todo el codigo
@@ -63,16 +67,22 @@ class Equipo extends EstadoBase {
         console.error("Error al buscar datos: ", error)
     }
   }
-  async buscarDatosSemana(tiempo, periodo1, periodo2){
-//TODO: Darle logica a buscar datos, agregar las graficas y arreglar todo el codigo
-        try{
-            const query= "SELECT SUM(monto) AS total, DATE_TRUNC('week', fecha) AS semana FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY semana ORDER BY semana"
-            const resultVentas= await pool.query(query, [periodo1, periodo2])
-            return resultVentas.rows
-        }catch(error){
-            console.error("Error al buscar datos: ", error)
-        }
+  async buscarDatosSemana(tiempo, periodo1, periodo2) {
+    // TODO: Darle lógica a buscar datos, agregar las gráficas y arreglar todo el código
+    try {
+        const queryVentas = `SELECT DATE_TRUNC('week', fecha) AS semana, SUM(coste_total) AS total FROM venta v INNER JOIN detalleventa dv ON v.id_venta = dv.id_venta WHERE fecha BETWEEN $1 AND $2 AND dv.tipo = 'Celular' GROUP BY semana ORDER BY semana`;
+        const resultVentas = await pool.query(queryVentas, [periodo1, periodo2]);
+
+        const result = resultVentas.rows.map((venta) => ({
+            semana: venta.semana,
+            total: venta.total || 0
+        }));
+
+        return result;
+    } catch (error) {
+        console.error("Error al buscar datos: ", error);
     }
+}
 
     async buscarDatosMes(tiempo, periodo1, periodo2){
 //TODO: Darle logica a buscar datos, agregar las graficas y arreglar todo el codigo
@@ -189,9 +199,15 @@ class FiltroState {
     }
   }
 
-  buscarDatos() {
-      this.estado.buscarDatos(this.tiempo, this.periodo1, this.periodo2, this);
-  }
+    buscarDatosDia() {
+        this.estado.buscarDatosDia(this.tiempo, this.periodo1, this.periodo2, this);
+    }
+    buscarDatosDia() {
+        this.estado.buscarDatosDia(this.tiempo, this.periodo1, this.periodo2, this);
+    }
+    buscarDatosDia() {
+        this.estado.buscarDatosDia(this.tiempo, this.periodo1, this.periodo2, this);
+    }
 }
 
 export default FiltroState;
