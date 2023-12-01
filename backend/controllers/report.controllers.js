2cd1dd02e470cf5f7a242f3176183e3bc2a5e74a
SergioCallan <sergio.callan@unmsm.edu.pe>
2023-12-01 14:01:08 -0500

    Primera version del calculo semanal de equipos

--- backend/controllers/report.controllers.js
+++ backend/controllers/report.controllers.js
@@ -43,14 +43,34 @@ const createGReportWeekly= async(req,res)=>{
     }
 }
 
-const createEReportDaily = async (req, res) => {
+const createEReportWeekly = async (req, res) => {
     try {
         const { periodo1, periodo2 } = req.body;
-        const queryVentas = "SELECT id_venta, fecha FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY id_venta, fecha ORDER BY fecha";
-        const resultVentas = await pool.query(queryVentas, [periodo1, periodo2]);
+
+        // Modificar las fechas para obtener el inicio y fin de las semanas
+        const startOfWeek1 = new Date(periodo1);
+        startOfWeek1.setDate(startOfWeek1.getDate() - startOfWeek1.getDay());
+        const endOfWeek2 = new Date(periodo2);
+        endOfWeek2.setDate(endOfWeek2.getDate() - endOfWeek2.getDay() + 6);
+
+        const queryVentas = `
+            SELECT id_venta, MIN(fecha) as start_date, MAX(fecha) as end_date
+            FROM venta
+            WHERE fecha BETWEEN $1 AND $2
+            GROUP BY id_venta, EXTRACT(WEEK FROM fecha)
+            ORDER BY start_date
+        `;
+
+        const resultVentas = await pool.query(queryVentas, [startOfWeek1, endOfWeek2]);
 
         const detallePromises = resultVentas.rows.map(async (venta) => {
-            const queryDetalles = "SELECT id_venta, SUM(coste_total) AS total FROM detalleventa WHERE tipo='Celular' AND id_venta= $1 GROUP BY id_venta";
+            const queryDetalles = `
+                SELECT id_venta, SUM(coste_total) AS total
+                FROM detalleventa
+                WHERE tipo='Celular' AND id_venta= $1
+                GROUP BY id_venta
+            `;
+
             const resultDetalles = await pool.query(queryDetalles, [venta.id_venta]);
             return resultDetalles.rows[0] || { id_venta: venta.id_venta, total: 0 };
         });
@@ -58,11 +78,12 @@ const createEReportDaily = async (req, res) => {
         const detallesTotales = await Promise.all(detallePromises);
 
         // Filtrar los detalles que tienen total diferente de 0
-        const detallesFiltrados = detallesTotales.filter((detallesTotales) => detallesTotales.total > 0);
+        const detallesFiltrados = detallesTotales.filter((detalle) => detalle.total > 0);
 
         const result = detallesFiltrados.map((detalle) => ({
             id_venta: detalle.id_venta,
-            fecha: resultVentas.rows.find((venta) => venta.id_venta === detalle.id_venta).fecha,
+            start_date: resultVentas.rows.find((venta) => venta.id_venta === detalle.id_venta).start_date,
+            end_date: resultVentas.rows.find((venta) => venta.id_venta === detalle.id_venta).end_date,
             total: detalle.total,
         }));
 
@@ -72,7 +93,6 @@ const createEReportDaily = async (req, res) => {
         throw error;
     }
 };
-
 const createPReportDaily= async(req, res)=>{
     try {
         const {periodo1, periodo2}= req.body
