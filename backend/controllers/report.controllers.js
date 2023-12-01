33e8737fea8452c60332ef9c6b2eb8870b7dfe8e
SergioCallan <sergio.callan@unmsm.edu.pe>
2023-12-01 14:17:05 -0500

    Correcciones

--- backend/controllers/report.controllers.js
+++ backend/controllers/report.controllers.js
@@ -77,29 +77,12 @@ const createEReportWeekly = async (req, res) => {
     try {
         const { periodo1, periodo2 } = req.body;
 
-        // Modificar las fechas para obtener el inicio y fin de las semanas
-        const startOfWeek1 = new Date(periodo1);
-        startOfWeek1.setDate(startOfWeek1.getDate() - startOfWeek1.getDay());
-        const endOfWeek2 = new Date(periodo2);
-        endOfWeek2.setDate(endOfWeek2.getDate() - endOfWeek2.getDay() + 6);
+        const queryVentas = `SELECT id_venta, MIN(fecha) as start_date, MAX(fecha) as end_date FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY id_venta, EXTRACT(WEEK FROM fecha) ORDER BY start_date`;
 
-        const queryVentas = `
-            SELECT id_venta, MIN(fecha) as start_date, MAX(fecha) as end_date
-            FROM venta
-            WHERE fecha BETWEEN $1 AND $2
-            GROUP BY id_venta, EXTRACT(WEEK FROM fecha)
-            ORDER BY start_date
-        `;
-
-        const resultVentas = await pool.query(queryVentas, [startOfWeek1, endOfWeek2]);
+        const resultVentas = await pool.query(queryVentas, [periodo1.start, periodo2.end]);
 
         const detallePromises = resultVentas.rows.map(async (venta) => {
-            const queryDetalles = `
-                SELECT id_venta, SUM(coste_total) AS total
-                FROM detalleventa
-                WHERE tipo='Celular' AND id_venta= $1
-                GROUP BY id_venta
-            `;
+            const queryDetalles = `SELECT id_venta, SUM(coste_total) AS total FROM detalleventa WHERE tipo='Celular' AND id_venta= $1 GROUP BY id_venta`;
 
             const resultDetalles = await pool.query(queryDetalles, [venta.id_venta]);
             return resultDetalles.rows[0] || { id_venta: venta.id_venta, total: 0 };
