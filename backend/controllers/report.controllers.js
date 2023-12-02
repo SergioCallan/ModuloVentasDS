382e3c56a6c8cc4ea41a13e46b5085f19ef15c92
SergioCallan <sergio.callan@unmsm.edu.pe>
2023-12-01 22:32:07 -0500

    Aplicando Strategy a Report.controllers

--- backend/controllers/report.controllers.js
+++ backend/controllers/report.controllers.js
@@ -1,247 +1,125 @@
-const pool= require ('../db')
+const pool = require('../db');
 
-const createGReportDaily= async(req,res)=>{
-    try{
-        const {periodo1, periodo2}= req.query
-        const query= "SELECT SUM(monto) AS total, fecha as start_date, fecha as end_date FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY fecha ORDER BY fecha"
-        const resultVentas= await pool.query(query, [periodo1, periodo2])
-        console.log(resultVentas.rows)
-        return res.json(resultVentas.rows)
-    }catch(error){
-        console.error("Error al buscar datos: ", error)
+class ReportStrategy {
+  async createReport(periodo1, periodo2, query) {
+    try {
+      const resultVentas = await pool.query(query, [periodo1, periodo2]);
+      // Lógica de procesamiento común
+      return resultVentas.rows;
+    } catch (error) {
+      console.error("Error al buscar datos: ", error);
+      throw error;
     }
+  }
 }
 
-const createGReportWeekly= async(req,res)=>{
-    try{
-        const {periodo1, periodo2}= req.query
-        const query= "SELECT DATE_TRUNC('week', fecha) AS semana, MIN(fecha) AS start_date, MAX(fecha) AS end_date, SUM(monto) AS total FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY semana ORDER BY semana"
-        const resultVentas= await pool.query(query, [periodo1, periodo2])
-        console.log(resultVentas.rows)
-        return res.json(resultVentas.rows)
-    }catch(error){
-        console.error("Error al buscar datos: ", error)
-    }
+class DailyReportStrategy extends ReportStrategy {
+  constructor() {
+    super();
+    this.query = "SELECT SUM(monto) AS total, fecha as start_date, fecha as end_date FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY fecha ORDER BY fecha";
+  }
 }
 
-const createGReportMonthly= async(req,res)=>{
-    try{
-        const {periodo1, periodo2}= req.query
-        const query= "SELECT DATE_TRUNC('month', fecha) AS mes, MIN(fecha) AS start_date, MAX(fecha) AS end_date, SUM(monto) AS total FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY mes ORDER BY mes"
-        const resultVentas= await pool.query(query, [periodo1, periodo2])
-        console.log(resultVentas.rows)
-        return res.json(resultVentas.rows)
-    }catch(error){
-        console.error("Error al buscar datos: ", error)
-    }
+class WeeklyReportStrategy extends ReportStrategy {
+  constructor() {
+    super();
+    this.query = "SELECT DATE_TRUNC('week', fecha) AS semana, MIN(fecha) AS start_date, MAX(fecha) AS end_date, SUM(monto) AS total FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY semana ORDER BY semana";
+  }
 }
 
-const createEReportDaily = async (req, res) => {
-    try {
-        const { periodo1, periodo2 } = req.query
-        const queryVentas = "SELECT id_venta, fecha AS start_date, fecha AS end_date FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY id_venta, fecha ORDER BY fecha";
-        const resultVentas = await pool.query(queryVentas, [periodo1, periodo2]);
-
-        const detallePromises = resultVentas.rows.map(async (venta) => {
-            const queryDetalles = "SELECT id_venta, SUM(coste_total) AS total FROM detalleventa WHERE tipo='Celular' AND id_venta= $1 GROUP BY id_venta";
-            const resultDetalles = await pool.query(queryDetalles, [venta.id_venta]);
-            return resultDetalles.rows[0] || { id_venta: venta.id_venta, total: 0 };
-        });
-
-        const detallesTotales = await Promise.all(detallePromises);
-
-        // Filtrar los detalles que tienen total diferente de 0
-        const detallesFiltrados = detallesTotales.filter((detallesTotales) => detallesTotales.total > 0);
-
-        const result = detallesFiltrados.map((detalle) => ({
-            id_venta: detalle.id_venta,
-            fecha: resultVentas.rows.find((venta) => venta.id_venta === detalle.id_venta).fecha,
-            total: detalle.total,
-        }));
-
-        return res.json(result);
-    } catch (error) {
-        console.error("Error al buscar datos: ", error);
-        throw error;
-    }
-};
-
-const createEReportWeekly = async (req, res) => {
-    try {
-        const { periodo1, periodo2 } = req.query;
-
-        const queryVentas = `SELECT id_venta, MIN(fecha) as start_date, MAX(fecha) as end_date FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY id_venta, EXTRACT(WEEK FROM fecha) ORDER BY start_date`;
-
-        const resultVentas = await pool.query(queryVentas, [periodo1, periodo2]);
-
-        const detallePromises = resultVentas.rows.map(async (venta) => {
-            const queryDetalles = `SELECT id_venta, SUM(coste_total) AS total FROM detalleventa WHERE tipo='Celular' AND id_venta= $1 GROUP BY id_venta`;
-            console.log(resultVentas)
-            const resultDetalles = await pool.query(queryDetalles, [venta.id_venta]);
-            return resultDetalles.rows[0] || { id_venta: venta.id_venta, total: 0 };
-        });
-
-        const detallesTotales = await Promise.all(detallePromises);
-
-        // Filtrar los detalles que tienen total diferente de 0
-        const detallesFiltrados = detallesTotales.filter((detalle) => detalle.total > 0);
-
-        const result = detallesFiltrados.map((detalle) => ({
-            id_venta: detalle.id_venta,
-            start_date: resultVentas.rows.find((venta) => venta.id_venta === detalle.id_venta).start_date,
-            end_date: resultVentas.rows.find((venta) => venta.id_venta === detalle.id_venta).end_date,
-            total: detalle.total,
-        }));
-        console.log(result)
-        return res.json(result);
-    } catch (error) {
-        console.error("Error al buscar datos: ", error);
-        throw error;
-    }
-};
-
-const createEReportMonthly = async (req, res) => {
-    try {
-        const { periodo1, periodo2 } = req.query;
-
-        const queryVentas = `SELECT id_venta, MIN(fecha) as start_date, MAX(fecha) as end_date FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY id_venta, EXTRACT(MONTH FROM fecha) ORDER BY start_date`;
-
-        const resultVentas = await pool.query(queryVentas, [periodo1, periodo2]);
-
-        const detallePromises = resultVentas.rows.map(async (venta) => {
-            const queryDetalles = `SELECT id_venta, SUM(coste_total) AS total FROM detalleventa WHERE tipo='Celular' AND id_venta= $1 GROUP BY id_venta`;
-            console.log(resultVentas)
-            const resultDetalles = await pool.query(queryDetalles, [venta.id_venta]);
-            return resultDetalles.rows[0] || { id_venta: venta.id_venta, total: 0 };
-        });
-
-        const detallesTotales = await Promise.all(detallePromises);
-
-        // Filtrar los detalles que tienen total diferente de 0
-        const detallesFiltrados = detallesTotales.filter((detalle) => detalle.total > 0);
-
-        const result = detallesFiltrados.map((detalle) => ({
-            id_venta: detalle.id_venta,
-            start_date: resultVentas.rows.find((venta) => venta.id_venta === detalle.id_venta).start_date,
-            end_date: resultVentas.rows.find((venta) => venta.id_venta === detalle.id_venta).end_date,
-            total: detalle.total,
-        }));
-        console.log(result)
-        return res.json(result);
-    } catch (error) {
-        console.error("Error al buscar datos: ", error);
-        throw error;
-    }
-};
-
-
-const createPReportDaily= async(req, res)=>{
-    try {
-        const {periodo1, periodo2}= req.query
-        const queryVentas = "SELECT id_venta, fecha AS start_date, fecha AS end_date FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY id_venta, fecha ORDER BY fecha";
-        const resultVentas = await pool.query(queryVentas, [periodo1, periodo2]);
-
-        const detallePromises = resultVentas.rows.map(async (venta) => {
-            const queryDetalles = "SELECT id_venta, SUM(coste_total) AS total FROM detalleventa WHERE tipo='Plan' AND id_venta= $1 GROUP BY id_venta";
-            const resultDetalles = await pool.query(queryDetalles, [venta.id_venta]);
-            return resultDetalles.rows[0] || { id_venta: venta.id_venta, total: 0 };
-        });
-
-        const detallesTotales = await Promise.all(detallePromises);
-
-        // Filtrar los detalles que tienen total diferente de 0
-        const detallesFiltrados = detallesTotales.filter((detallesTotales) => detallesTotales.total > 0);
-
-        const result = detallesFiltrados.map((detalle) => ({
-            id_venta: detalle.id_venta,
-            fecha: resultVentas.rows.find((venta) => venta.id_venta === detalle.id_venta).fecha,
-            total: detalle.total,
-        }));
-
-        return res.json(result);
-    } catch (error) {
-        console.error("Error al buscar datos: ", error);
-        throw error;
-    }
+class MonthlyReportStrategy extends ReportStrategy {
+  constructor() {
+    super();
+    this.query = "SELECT DATE_TRUNC('month', fecha) AS mes, MIN(fecha) AS start_date, MAX(fecha) AS end_date, SUM(monto) AS total FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY mes ORDER BY mes";
+  }
 }
 
-const createPReportWeekly = async (req, res) => {
-    try {
-        const { periodo1, periodo2 } = req.query
-
-        const queryVentas = `SELECT id_venta, MIN(fecha) as start_date, MAX(fecha) as end_date FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY id_venta, EXTRACT(WEEK FROM fecha) ORDER BY start_date`;
-
-        const resultVentas = await pool.query(queryVentas, [periodo1, periodo2]);
-
-        const detallePromises = resultVentas.rows.map(async (venta) => {
-            const queryDetalles = `SELECT id_venta, SUM(coste_total) AS total FROM detalleventa WHERE tipo='Plan' AND id_venta= $1 GROUP BY id_venta`;
-            console.log(resultVentas)
-            const resultDetalles = await pool.query(queryDetalles, [venta.id_venta]);
-            return resultDetalles.rows[0] || { id_venta: venta.id_venta, total: 0 };
-        });
-
-        const detallesTotales = await Promise.all(detallePromises);
-
-        // Filtrar los detalles que tienen total diferente de 0
-        const detallesFiltrados = detallesTotales.filter((detalle) => detalle.total > 0);
-
-        const result = detallesFiltrados.map((detalle) => ({
-            id_venta: detalle.id_venta,
-            start_date: resultVentas.rows.find((venta) => venta.id_venta === detalle.id_venta).start_date,
-            end_date: resultVentas.rows.find((venta) => venta.id_venta === detalle.id_venta).end_date,
-            total: detalle.total,
-        }));
-        console.log(result)
-        return res.json(result);
-    } catch (error) {
-        console.error("Error al buscar datos: ", error);
-        throw error;
-    }
-};
-
-const createPReportMonthly = async (req, res) => {
-    try {
-        const { periodo1, periodo2 } = req.query
-
-        const queryVentas = `SELECT id_venta, MIN(fecha) as start_date, MAX(fecha) as end_date FROM venta WHERE fecha BETWEEN $1 AND $2 GROUP BY id_venta, EXTRACT(MONTH FROM fecha) ORDER BY start_date`;
-
-        const resultVentas = await pool.query(queryVentas, [periodo1, periodo2]);
-
-        const detallePromises = resultVentas.rows.map(async (venta) => {
-            const queryDetalles = `SELECT id_venta, SUM(coste_total) AS total FROM detalleventa WHERE tipo='Plan' AND id_venta= $1 GROUP BY id_venta`;
-            console.log(resultVentas)
-            const resultDetalles = await pool.query(queryDetalles, [venta.id_venta]);
-            return resultDetalles.rows[0] || { id_venta: venta.id_venta, total: 0 };
-        });
-
-        const detallesTotales = await Promise.all(detallePromises);
-
-        // Filtrar los detalles que tienen total diferente de 0
-        const detallesFiltrados = detallesTotales.filter((detalle) => detalle.total > 0);
-
-        const result = detallesFiltrados.map((detalle) => ({
-            id_venta: detalle.id_venta,
-            start_date: resultVentas.rows.find((venta) => venta.id_venta === detalle.id_venta).start_date,
-            end_date: resultVentas.rows.find((venta) => venta.id_venta === detalle.id_venta).end_date,
-            total: detalle.total,
-        }));
-        console.log(result)
-        return res.json(result);
-    } catch (error) {
-        console.error("Error al buscar datos: ", error);
-        throw error;
-    }
-};
-
-
-module.exports={
-    createGReportDaily,
-    createGReportWeekly,
-    createGReportMonthly,
-    createEReportDaily,
-    createEReportWeekly,
-    createEReportMonthly,
-    createPReportDaily,
-    createPReportWeekly,
-    createPReportMonthly
+class EquipmentReportStrategy extends ReportStrategy {
+  constructor() {
+    super();
+    this.query = "SELECT DATE_TRUNC('week', fecha) AS semana, MIN(fecha) AS start_date, MAX(fecha) AS end_date, SUM(monto) AS total FROM venta WHERE fecha BETWEEN $1 AND $2 AND tipo = 'Equipo' GROUP BY semana ORDER BY semana";
+  }
 }
+
+class PlanReportStrategy extends ReportStrategy {
+  constructor() {
+    super();
+    this.query = "SELECT DATE_TRUNC('week', fecha) AS semana, MIN(fecha) AS start_date, MAX(fecha) AS end_date, SUM(monto) AS total FROM venta WHERE fecha BETWEEN $1 AND $2 AND tipo = 'Plan' GROUP BY semana ORDER BY semana";
+  }
+}
+
+const createReport = async (req, res, strategy) => {
+  const { periodo1, periodo2 } = req.query;
+  const reportStrategy = new strategy();
+
+  const result = await reportStrategy.createReport(periodo1, periodo2, reportStrategy.query);
+
+  res.json(result);
+};
+
+const createEReportDaily = async (req, res, strategy) => {
+  const { periodo1, periodo2 } = req.query;
+  const reportStrategy = new strategy();
+
+  const result = await reportStrategy.createReport(periodo1, periodo2, reportStrategy.query);
+
+  res.json(result);
+};
+
+const createEReportWeekly = async (req, res, strategy) => {
+  const { periodo1, periodo2 } = req.query;
+  const reportStrategy = new strategy();
+
+  const result = await reportStrategy.createReport(periodo1, periodo2, reportStrategy.query);
+
+  res.json(result);
+};
+
+const createEReportMonthly = async (req, res, strategy) => {
+  const { periodo1, periodo2 } = req.query;
+  const reportStrategy = new strategy();
+
+  const result = await reportStrategy.createReport(periodo1, periodo2, reportStrategy.query);
+
+  res.json(result);
+};
+
+const createPReportDaily = async (req, res, strategy) => {
+  const { periodo1, periodo2 } = req.query;
+  const reportStrategy = new strategy();
+
+  const result = await reportStrategy.createReport(periodo1, periodo2, reportStrategy.query);
+
+  res.json(result);
+};
+
+const createPReportWeekly = async (req, res, strategy) => {
+  const { periodo1, periodo2 } = req.query;
+  const reportStrategy = new strategy();
+
+  const result = await reportStrategy.createReport(periodo1, periodo2, reportStrategy.query);
+
+  res.json(result);
+};
+
+const createPReportMonthly = async (req, res, strategy) => {
+  const { periodo1, periodo2 } = req.query;
+  const reportStrategy = new strategy();
+
+  const result = await reportStrategy.createReport(periodo1, periodo2, reportStrategy.query);
+
+  res.json(result);
+};
+
+module.exports = {
+  createGReportDaily: (req, res) => createReport(req, res, DailyReportStrategy),
+  createGReportWeekly: (req, res) => createReport(req, res, WeeklyReportStrategy),
+  createGReportMonthly: (req, res) => createReport(req, res, MonthlyReportStrategy),
+  createEReportDaily: (req, res) => createEReportDaily(req, res, EquipmentReportStrategy),
+  createEReportWeekly: (req, res) => createEReportWeekly(req, res, EquipmentReportStrategy),
+  createEReportMonthly: (req, res) => createEReportMonthly(req, res, EquipmentReportStrategy),
+  createPReportDaily: (req, res) => createPReportDaily(req, res, PlanReportStrategy),
+  createPReportWeekly: (req, res) => createPReportWeekly(req, res, PlanReportStrategy),
+  createPReportMonthly: (req, res) => createPReportMonthly(req, res, PlanReportStrategy),
+  // ... otras funciones de informe
+};
